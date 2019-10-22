b07274a38df487f38c921940cf7fdfe14427f911
Matt <ybotspawn.com>
2019-10-21 21:59:48 -0400

    Added note regarding issue with app password

--- kwrapper/ksetup.py
+++ kwrapper/ksetup.py
@@ -1,2 +1,3 @@
 # Placeholder for setup file
 # file will interface with both gnome keyring and google to get an app password, if necessary, and store it
+#https://github.com/kiwiz/gkeepapi/issues/20
